0565c111f6ae33613f52dd87dfcb3361f99ef510
caihongwang <caihongwang@dingtalk.com>
2018-09-12 10:07:28 +0800

    对网络请求模块进行优化，当登录过期时重新登录并在请求.

--- utils/network.js
+++ utils/network.js
@@ -26,14 +26,12 @@ function POST(requestHandler) {
     }
     request('POST', requestHandler, header);
 }
-//发送请求基类
+//执行发送请求
 function request(method, requestHandler, header) {
     var params = requestHandler.params;
     params.sessionKey = wx.getStorageSync("SESSIONKEY");
     params.uid = wx.getStorageSync("UIDKEY");
     var requestUrl = requestHandler.requestUrl;
-    // console.log('发送的网络请求:' + requestUrl);
-    // console.log(params);
     //这里还可以对公共参数做统一处理
     wx.request({
         url: requestUrl,
@@ -42,30 +40,31 @@ function request(method, requestHandler, header) {
         header: header,
         success: function (res) {
             //这里还可以对3rd_session过期做处理
-            // console.log('发送接收的code（' + requestUrl + '）:' + res.data.code + "接收的message:" + res.data.message);
-            if (res.data.code == 10002) {    //session过期的处理，重新登录
-                if (params.sessionCheck && params.sessionCheck == 1) {   //如果是请求session过期处理，就不在这里操作
-                    requestHandler.success(res);
-                    return;
-                }
-                // console.log("请求过程中Session过期");
-                //这里会重新调用登录
-                clearInfo();
-                goLogin();
+            console.log('发送接收的code（' + requestUrl + '）:' + res.data.code + "接收的message:" + res.data.message);
+            if (res.data.code == 10002 || res.data.code == 10003) {    //session过期的处理，重新登录
+              if (params.sessionCheck && params.sessionCheck == 1) {   //如果是请求session过期处理，就不在这里操作
+                requestHandler.success(res);
+                return;
+              }
+              // console.log("请求过程中Session过期");
+              //这里会重新调用登录
+              clearInfo();
+              goLogin(request, method, requestHandler, header);
+            } else {
+              requestHandler.success(res);
             }
-            requestHandler.success(res);
         },
         fail: function (res) {
             requestHandler.fail(res);
         },
         complete: function (res) {
             if (res.statusCode != 200) {
-                console.log(res);
+              console.log(res);
             }
         }
-    })
+    });
 }
-function goLogin() {    //私有的请求方法，不准在外部调用
+function goLogin(Func, method, requestHandler, header) {    //私有的请求方法，不准在外部调用
     wx.login({
         success: function (res1) {
             //调用登录
@@ -80,17 +79,22 @@ function goLogin() {    //私有的请求方法，不准在外部调用
                             title: '提示',
                             content: res.data.message,
                             showCancel: false
-                        })
+                        });
                         return;
                     }
                     // console.log("登录成功");
                     //登录成功，将uid和session保存
                     saveInfo(res.data.data.sessionKey, res.data.data.uid);
-                    // console.log(res.data.data.sessionKey + "-----" + res.data.data.uid);
+                    console.log("【准备】开始执行后callback");
+                    if (Func != null && Func != "" && method != null && method != "" &&
+                      requestHandler != null && requestHandler != "" && header != null && header != ""){
+                        console.log("开始执行后callback");
+                        Func(method, requestHandler, header);
+                    }
                 }
-            })
+            });
         }
-    })
+    });
 }
 function saveInfo(session, uid) {    //私有方法，不准在外部调用。将重新登录的数据保存
     // console.log("开始保存用户信息");
